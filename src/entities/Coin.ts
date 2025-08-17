0d09327fc536c4bb75269ddb53dc72bf07276acb
Daniel Whiffing <danielwhiffing@gmail.com>
2025-08-16 20:56:31 -0400

    Adds coin pickup particles

--- src/entities/Coin.ts
+++ src/entities/Coin.ts
@@ -5,11 +5,24 @@ export class Coin extends Physics.Arcade.Sprite {
   protected sceneRef: Game
   public lifetime = 20
   private fadeTween?: Phaser.Tweens.Tween
+  particles: Phaser.GameObjects.Particles.ParticleEmitter
 
   constructor(scene: Game) {
     super(scene, 3 * 8, 3 * 8, 'sheet', 3)
     this.sceneRef = scene
     scene.add.existing(this)
+
+    this.particles = scene.add
+      .particles(4, 4, 'sheet', {
+        frame: 3,
+        lifespan: 300,
+        speed: 40,
+        scale: 0.7,
+        alpha: { start: 0.4, end: 0 },
+        tint: 0xffaa00,
+        angle: { start: 0, end: 360, steps: 6 },
+      })
+      .stop()
     scene.physics.add.existing(this)
 
     this.setOrigin(0).setSize(3, 3).setOffset(3, 2).setTint(0xffaa00)
@@ -28,6 +41,7 @@ export class Coin extends Physics.Arcade.Sprite {
 
   public pickup() {
     this.setActive(false).setVisible(false)
+    this.particles.setPosition(this.x + 4, this.y + 4).explode(6)
     this.fadeTween?.remove()
   }
 
